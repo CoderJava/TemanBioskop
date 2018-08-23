a9383add5b6e49f5ea9b4a5ddc10e901f6eef735
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-08-23 23:38:01 +0700

    Reset project

--- app/build/generated/source/apt/debug/ysn/temanbioskop/BR.java
+++ app/build/generated/source/apt/debug/ysn/temanbioskop/BR.java
@@ -2,5 +2,4 @@ package ysn.temanbioskop;
 
 public class BR {
         public static final int _all = 0;
-        public static final int detail_movie_discover = 1;
 }
--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="e513b891-d2ba-41ff-a8ce-3cbe78f05437", modulePackage="ysn.temanbioskop", sdkRoot="/home/yudi/Android/Sdk",layoutInfoDir="/home/yudi/AndroidStudioProjects/TemanBioskop/app/build/intermediates/data-binding-info/debug",exportClassListTo="",isLibrary=false,minSdk=17,enableDebugLogs=false,printEncodedError=true)
+@BindingBuildInfo(buildId="38b8d27a-ce3e-427b-9b7a-922b999836c1", modulePackage="ysn.temanbioskop", sdkRoot="/Users/yudisetiawan/Library/Android/sdk",layoutInfoDir="/Users/yudisetiawan/AndroidStudioProjects/TemanBioskop/app/build/intermediates/data-binding-info/debug",exportClassListTo="",isLibrary=false,minSdk=17,enableDebugLogs=false,printEncodedError=true)
 public class DataBindingInfo {}
