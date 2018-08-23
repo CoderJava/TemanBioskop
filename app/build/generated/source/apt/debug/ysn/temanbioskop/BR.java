d3d2b8e3820661d9830eb3442771abf00eff01df
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-08-24 00:04:19 +0700

    Initial project for MVVM design pattern

--- app/build/generated/source/apt/debug/ysn/temanbioskop/BR.java
+++
@@ -1,5 +0,0 @@
-package ysn.temanbioskop;
-
-public class BR {
-        public static final int _all = 0;
-}
--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="38b8d27a-ce3e-427b-9b7a-922b999836c1", modulePackage="ysn.temanbioskop", sdkRoot="/Users/yudisetiawan/Library/Android/sdk",layoutInfoDir="/Users/yudisetiawan/AndroidStudioProjects/TemanBioskop/app/build/intermediates/data-binding-info/debug",exportClassListTo="",isLibrary=false,minSdk=17,enableDebugLogs=false,printEncodedError=true)
-public class DataBindingInfo {}
